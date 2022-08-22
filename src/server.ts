3a885633e425c760e2af1f39d8069a3f50d46427
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2022-08-22 19:15:34 -0300

    feat: ✨ debugging aplication

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
-import express, { response } from "express";
+import express from "express";
 
- const app = express();
+const app = express();
 
- app.get("/", (request, response) => {
-   return response.json({ message: "Hello World" });
- });
+app.use(express.json());
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
 
- app.listen(3333, () => {
-   console.log("🚀 Server is running");
- });
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ message: `Hello ${name}` });
+});
+
+app.listen(3333, () => {
+  console.log("🚀 Server is running");
+});
